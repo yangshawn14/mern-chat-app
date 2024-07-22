340703e9f47627977e4fb32645d79a9c490b74b2
Shawn Yang <yangshawn14@gmail.com>
2024-07-21 20:29:56 -0600

    Change socket url to render url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://mern-chat-app-14up.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
